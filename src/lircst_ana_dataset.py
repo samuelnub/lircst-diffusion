Fixed normalisation for scatter channel which was causing poor training signal

src/lircst_ana_dataset.py
@@ -46,10 +46,34 @@ class LircstAnaDataset(Dataset):
             # This is done by first converting to float32,
             # Dividing by half of the max value of the tensor, and then subtracting 1
             phan = phan.astype(np.float32)
-            phan[0] = phan[0] / (np.max(phan[0]) / 2) - 1
-            phan[1] = phan[1] / (np.max(phan[1]) / 2) - 1
             sino = sino.astype(np.float32)
-            sino = sino / (np.max(sino) / 2) - 1
+
+            # The scatter map is normalised slightly differently
+            # We scale non-zeroes to [0, 1] and zeroes to -1
+
+            nonzero_phan0 = np.nonzero(phan[0])
+
+            nonzero_min_phan0 = np.min(phan[0][nonzero_phan0])
+            nonzero_max_phan0 = np.max(phan[0][nonzero_phan0])
+
+            min_phan1 = np.min(phan[1])
+            max_phan1 = np.max(phan[1])
+
+            min_sino = np.min(sino)
+            max_sino = np.max(sino)
+
+            if not nonzero_max_phan0 == nonzero_min_phan0:
+                phan[0][nonzero_phan0] = (phan[0][nonzero_phan0] - nonzero_min_phan0) / (nonzero_max_phan0 - nonzero_min_phan0)
+                phan[0][np.where(phan[0] == 0.0)] = -1.0
+            else:
+                min_phan0 = np.min(phan[0])
+                max_phan0 = np.max(phan[0])
+                phan[0] = ((phan[0] - min_phan0) / (max_phan0 - min_phan0)) * 2 - 1
+
+            phan[1] = ((phan[1] - min_phan1) / (max_phan1 - min_phan1)) * 2 - 1
+            sino = ((sino - min_sino) / (max_sino - min_sino)) * 2 - 1
+
+
 
         if self.transform_phan:
             phan = self.transform_phan(phan)
@@ -58,3 +82,10 @@ class LircstAnaDataset(Dataset):
 
         return phan, sino, phantom_id
 
+    def get_phan_metadata(self, phantom_id: str) -> dict:
+        # Load the metadata for the given phantom_id
+        phantom_dir = os.path.join(self.data_dir, phantom_id)
+        meta_path = os.path.join(phantom_dir, 'meta.npy')
+        if not os.path.exists(meta_path):
+            raise FileNotFoundError(f'Metadata file not found for phantom_id {phantom_id}')
+        return np.load(meta_path, allow_pickle=True).item()