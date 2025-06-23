2d8ef753b6aaf764570e3f8aef40bf6a03e1f754
Sam Yap <s.r.yap@surrey.ac.uk>
2025-06-23 19:30:42 +0100

    incorporating discretised forward model(s) wip

---
+++ src/physics_inc.py
@@ -0,0 +1,26 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from Diffusion.DenoisingDiffusionProcess.forward import GaussianForwardProcess
+
+class PhysicsIncorporated(nn.Module):
+    def __init__(self, 
+                 gaussian_forward_process: GaussianForwardProcess,
+                 forward_operator_dir: str):
+        self.gaussian_forward_process = gaussian_forward_process
+
+        # TODO:
+        # Load the forward operator from the specified directory
+
+    def forward(self, x_t: torch.Tensor, noise_hat: torch.Tensor, t):
+        # Apply forward operator to our predicted x_0 based on x_t and noise_hat
+        pass
+
+def extract(a, t, x_shape):
+    # https://github.com/Stability-AI/stablediffusion/blob/main/ldm/modules/diffusionmodules/util.py#L103
+    # https://github.com/lucidrains/denoising-diffusion-pytorch/blob/1d9d8dffb72e02172da8a77bee039b1c72b7c6d5/denoising_diffusion_pytorch/repaint.py#L431
+    b, *_ = t.shape
+    out = a.gather(-1, t)
+    return out.reshape(b, *((1,) * (len(x_shape) - 1)))
--- src/sampler_wrapper.py
+++ src/sampler_wrapper.py
@@ -7,7 +7,8 @@ class SamplerWrapper():
     def __init__(self, sample_timesteps=200, train_timesteps=1000):
         self.sample_timesteps = sample_timesteps
         self.train_timesteps = train_timesteps # TODO VVVVVVVV self.sample_timesteps is used for sampling
-        self.sampler = DDIM_Sampler(num_timesteps=self.train_timesteps, train_timesteps=self.train_timesteps)
+        self.sampler = DDPM_Sampler(num_timesteps=self.train_timesteps)
+        # DDIM_Sampler(num_timesteps=self.train_timesteps, train_timesteps=self.train_timesteps)
 
     def get_sampler(self) -> DDIM_Sampler | DDPM_Sampler:
         return self.sampler
